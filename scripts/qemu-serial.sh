8327d026875719265f3970e2180fc650bf4a69fb
slonkazoid <alifurkanyildiz@gmail.com>
2024-01-28 17:48:39 +0300

    add qemu-serial.sh for freedos

---
+++ scripts/qemu-serial.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# use all threads and quarter of available system memory
+threads="$(nproc)"
+mem="$(($(grep MemTotal /proc/meminfo | awk '{print $2}') / 4096))"
+image_path="/tmp/funny.iso"
+
+qemu="qemu-system-x86_64"
+flags="-boot d -cdrom $image_path -m $mem -smp $threads -nographic"
+[ -e /dev/kvm ] && flags="$flags -accel kvm" # enable kvm if available
+
+extract > "$image_path"
+
+clear
+$qemu $flags
+rm "$image_path"
